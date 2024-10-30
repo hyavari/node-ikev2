9447e39e8239a5129ee4bb9fbb01b1d844658d76
Hossein Yavari <hyavari26@gmail.com>
2024-10-30 16:29:46 -0400

    removing the lib

--- lib/index.d.ts
+++
@@ -1,8 +0,0 @@
-export * from "./attribute";
-export * from "./payload";
-export * from "./header";
-export * from "./proposal";
-export * from "./selector";
-export * from "./transform";
-import * as ikev2 from "./message";
-export { ikev2 };
